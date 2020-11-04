c2517b406df233d90c4810427628c387f6a72743
YOYITsM3M8 <49005044+YOYITsM3M8@users.noreply.github.com>
2020-11-03 19:13:06 -0500

    Update [id].js

--- pages/image/[id].js
+++ pages/image/[id].js
@@ -1,41 +1,12 @@
 import Layout from '../../components/layout'
 import Link from 'next/link'
-import { getAllPostIds, getPostData } from '../../lib/posts'
+import { getImage } from '../../lib/images'
 import Head from 'next/head'
 import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
 
 export default function Post({ postData }) {
   return (
-    <Layout>
-      <head>
-        <title>{postData.title}</title>
-      </head>
-      <article>
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-        <Link href="/">↵ Back</Link>
-        <div className={utilStyles.lightText}>
-          <Date dateString={postData.date} />
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-      </article>
-    </Layout>
+    <img src="fullPath" alt="image" />
   )
-}
-
-export async function getStaticPaths() {
-  const paths = getAllPostIds()
-  return {
-    paths,
-    fallback: false
-  }
-}
-
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id)
-  return {
-    props: {
-      postData
-    }
-  }
 }
